d42c0b28086e2b2da5c0edd8b170ae7766299347
Parv-gugnani <9765.parv@gmail.com>
2023-07-06 13:06:22 +0530

    calculatorwithreqt

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./style.css";
+
+function App() {
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">22132</div>
+        <div className="current-operand">22132</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>+</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
+    </div>
+  );
+}
+
+export default App;
+/*export default is used to export a 
+single class, function or primitive 
+from a script file. The export can 
+also be written as export default class 
+HelloWorld extends*/
